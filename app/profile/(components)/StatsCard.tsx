3af56fe70233a1260fb99cc378ef1b55adb7a82f
0nlyssslatts <sid36205@mail.ru>
2025-12-12 09:43:47 +0300

    vercel test

--- app/profile/(components)/StatsCard.tsx
+++ app/profile/(components)/StatsCard.tsx
@@ -5,7 +5,6 @@ interface StatsCardProps {
 export default function StatsCard({ bonusBalance }: StatsCardProps) {
   return (
     <div className="flex gap-1 h-[117px]">
-      {/* Bonuses Section */}
       <div className="flex-1 bg-white rounded-2xl shadow-sm p-4 flex flex-col justify-between">
         <p className="text-4xl font-semibold">{bonusBalance}</p>
         <p className="text-base text-black">бонусов</p>
